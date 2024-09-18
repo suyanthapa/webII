a26d3cb2966c0461689be27fd1304d47d8a41b3c
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2024-09-18 09:43:24 +0545

    first commit

---
+++ 17sept.js
@@ -0,0 +1,13 @@
+let element= document.createElement("div");
+element.innerText="hello world";
+
+element.style.backgroundColor= "red";
+element.style.color=" white";
+document.body.append(element);
+
+let c1= document.getElementsByClassName("c1")[1];
+
+c1.style.backgroundColor="Blue";
+
+// let ktm= document.getElementById('ktm');
+// ktm.style.display="none";
